657531a22bb9c7dda5c165e7257ec151974331f8
Unhurian Oleksandr <unguransasha@gmail.com>
2025-04-17 03:56:35 +0300

    Update task1_1.cpp

--- Lab1/task1_1.cpp
+++ Lab1/task1_1.cpp
@@ -8,5 +8,6 @@ void task_1(std::vector<int>& array_a, std::vector<int>& array_b, std::vector<in
     for (int i = 0; i < (array_a.size() / 2); i++) array_c.push_back(array_a[i] + array_b[i]);
     for (int j = array_b.size() / 2; j < array_b.size(); j++) array_c.push_back(array_a[j] - array_b[j]);
 
+    cin.ignore();
     std::cin.get();
 }
