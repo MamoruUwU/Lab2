b32d461ba3dfc1f065c2be8aef3ab8d908346753
Unhurian Oleksandr <unguransasha@gmail.com>
2025-04-17 09:36:28 +0300

    456

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -16,6 +16,7 @@ int main() {
     srand(time(nullptr));
 
     bool flag = true;
+
     do {
         int highlight = lab1_visual();
         switch (highlight - 6)
@@ -98,7 +99,7 @@ int main() {
             flag = false;
             break;
         }
-        //asdfsad123
+        //asdfsad123456
         }
 
     } while (flag == true);
